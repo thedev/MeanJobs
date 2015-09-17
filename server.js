4d753c320876fc398ac2e1f091a9d065325916fa
Alexandru Ionica <developer.lex@gmail.com>
2015-09-17 16:43:41 +0100

    load data from mongodb

--- server.js
+++ server.js
@@ -31,6 +31,12 @@ db.on('error', console.error.bind(console,'connection err ...'));
 db.once('open', function callback(){
 	console.log('meanjobs db open');
 });
+var messageSchema = mongoose.Schema({message: String});
+var Message = mongoose.model('Message',messageSchema);
+var mongoMessage;
+Message.findOne().exec(function(err, messageDoc){
+	mongoMessage = messageDoc.message;
+});
 
 
 app.get('/partials/:partialPath', function(req, res){
@@ -39,7 +45,9 @@ app.get('/partials/:partialPath', function(req, res){
 
 //client responsible for routing instead of coordinating routes
 app.get('*', function (req, res){
-	res.render('index');
+	res.render('index', {
+		mongoMessage: mongoMessage
+	});
 });
 
 var port = 3030;
